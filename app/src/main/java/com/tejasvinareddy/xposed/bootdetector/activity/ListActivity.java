5115488b6f05b03b46d1e69269de987efa877b34
Tejasvi Nareddy <tnareddy@salesforce.com>
2016-06-01 23:35:40 -0700

    Bug fixes, small UI changes, better documentations, and debugging.

--- app/src/main/java/com/tejasvinareddy/xposed/bootdetector/activity/ListActivity.java
+++ app/src/main/java/com/tejasvinareddy/xposed/bootdetector/activity/ListActivity.java
@@ -5,10 +5,12 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import com.tejasvinareddy.xposed.bootdetector.R;
 import com.tejasvinareddy.xposed.bootdetector.model.AppWrapper;
 import com.tejasvinareddy.xposed.bootdetector.ui.AppWrapperAdapter;
+import de.robv.android.xposed.XposedBridge;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,7 +35,9 @@ public class ListActivity extends AppCompatActivity {
         // DEBUG
         count++;
         appMap.put("Test" + count, new AppWrapper("Test" + count));
+        Log.d("DEBUG", "Added in debug entry");
 
+        // Set up the adapter
         // FIXME list
         adapter = new AppWrapperAdapter(appMap,
                 new AppWrapperAdapter.FeedInteractionListener() {
@@ -41,11 +45,11 @@ public class ListActivity extends AppCompatActivity {
                     @Override
                     public void onPostClicked(AppWrapper app, int index) {
                         // TODO
-
                     }
                 });
-
         recyclerView.setAdapter(adapter);
+
+        // DEBUG
         Log.d("List", "Refreshed!");
     }
 
@@ -54,11 +58,17 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        // FIXME
+        // Set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         setTitle("Xposed Boot Detector");
 
+        // Set up recycler view
         recyclerView = (RecyclerView) findViewById(R.id.app_list_recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
+        // Set up swipe-up-to-refresh layout
         srl = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
         srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -69,15 +79,15 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
+        // Reset the recycler view to set the adapter
         refreshRecyclerView();
     }
 
     public static Map<String, AppWrapper> getAppMap() {
         // FIXME encapsulation
+        // DEBUG
         Log.d("List", "Returned map");
+        XposedBridge.log("[BootDetector] Returned map!");
         return appMap;
     }
 }
